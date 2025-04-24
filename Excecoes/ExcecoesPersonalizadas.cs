fdfa6627f6f6282d2a7b763b4e2a6708c7801a19
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2025-04-23 21:04:23 -0300

    Feito por Upload

---
+++ Excecoes/ExcecoesPersonalizadas.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Excecoes
+{
+    public class NegativoExcecao : Exception
+    {
+        public NegativoExcecao() { }
+
+        public NegativoExcecao(string menssage) : base(menssage) { }
+
+        public NegativoExcecao(string menssage, Exception inner) : base(menssage, inner) { }
+
+    }
+
+    public class ImparExcecao : Exception
+    {
+        public ImparExcecao(string menssage) : base(menssage) { }
+
+    }
+
+    class ExcecoesPersonalizadas
+    {
+        public static int PositivoPar()
+        {
+            Random random = new Random();
+            int valor = random.Next(-30, 30);
+
+            if (valor < 0)
+            {
+                throw new NegativoExcecao("Numero negativo... :(");
+            }
+
+            if(valor % 2 == 1)
+            {
+                throw new ImparExcecao("Valor impar... :(");
+            }
+
+            return valor;
+        }
+
+        public static void Executar()
+        {
+            try
+            {
+                Console.WriteLine(PositivoPar());
+            }
+            catch (NegativoExcecao ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ImparExcecao ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Excecoes/PrimeiraExcecao.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Excecoes
+{
+    class PrimeiraExcecao
+    {
+        public class Conta
+        {
+            double Saldo;
+
+            public Conta(double Saldo)
+            {
+                Saldo = Saldo;
+            }
+
+            public void Sacar(double valor)
+            {
+                if (valor > Saldo)
+                {
+                    throw new ArgumentException("Saldo Insuficente.");
+                }
+
+                Saldo -= valor;
+            }
+        }
+
+        public static void Executar()
+        {
+            var conta = new Conta(1_223.45);
+
+            try
+            {
+                conta.Sacar(1600);
+                Console.WriteLine("Saque com sucesso! ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado! ");
+            }
+        }
+    }
+}
